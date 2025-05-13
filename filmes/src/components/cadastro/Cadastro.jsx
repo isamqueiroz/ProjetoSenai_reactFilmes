99185245bcccf573c06e9634bd0752c15aa64fed
Isadora Macedo <isa22queiroz@gmail.com>
2025-05-13 16:53:16 -0300

    feat: subindo

--- filmes/src/components/cadastro/Cadastro.jsx
+++ filmes/src/components/cadastro/Cadastro.jsx
@@ -1,31 +1,42 @@
 import "./Cadastro.css";
-import Botao from "../botao/Botao"
+import Botao from "../botao/Botao";
 
 const Cadastro = (props) => {
-    return(
-        <section className="section_cadastro">
-            <form action="" className="layout_grid form_cadastro">
-                <h1>{props.tituloCadastro}</h1>
-                <hr/> 
-                <div className="campos_cadastro">
-                    <div className="campo_cad_nome">
-                        <label htmlFor="nome"></label>
-                        <input type="text" name="nome" placeholder={`Digite o nome do ${props.placeholder}`}/>
-                    </div>
-                    <div className="campo_cad_genero" style={{display: props.visibilidade}}>
-                        <label htmlFor="genero"></label>
-                        <select name="genero" id="">
-                           <option value="" disabled selected>Selecione</option>
-                           <option value="">op 1</option>
-                           <option value="">op 2</option>
-                           <option value="">op 3</option>
-                        </select>
-                    </div>
-                    <Botao nomeDoBotao="Cadastrar"/>
-                </div>
-            </form>
-        </section>
-    )
-}
+  return (
+    <section className="section_cadastro">
+      <form onSubmit={props.funcCadastro} className="layout_grid form_cadastro">
+        <h1>{props.tituloCadastro}</h1>
+        <hr />
+        <div className="campos_cadastro">
+          <div className="campo_cad_nome">
+            <label htmlFor="nome"></label>
+            <input
+              type="text"
+              name="nome"
+              placeholder={`Digite o nome do ${props.placeholder}`}
+              value={props.valorInput}
+               onChange={(e) => props.setValorInput(e.target.value) }
+            />
+          </div>
+          <div
+            className="campo_cad_genero"
+            style={{ display: props.visibilidade }}
+          >
+            <label htmlFor="genero"></label>
+            <select name="genero" id="">
+              <option value="" disabled selected>
+                Selecione
+              </option>
+              <option value="">op 1</option>
+              <option value="">op 2</option>
+              <option value="">op 3</option>
+            </select>
+          </div>
+          <Botao nomeDoBotao="Cadastrar" />
+        </div>
+      </form>
+    </section>
+  );
+};
 
 export default Cadastro;
--- filmes/src/pages/cadastroGenero/CadastroGenero.jsx
+++ filmes/src/pages/cadastroGenero/CadastroGenero.jsx
@@ -1,26 +1,88 @@
+import { useEffect, useState } from "react";
+
+//import components
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import Cadastro from "../../components/cadastro/Cadastro";
 import Lista from "../../components/lista/Lista";
 
+import api from "../../Services/services"
+
 const CadastroGenero = () => {
-  return (
-    <>
-      <Header />
-      <main>
-        <Cadastro tituloCadastro="Cadastro de Gênero"
-        visibilidade = "none"
-        placeholder ="Gênero"
-      
-         />
-        <Lista 
-          ListaTitulo ="Lista de Gênero"
-          visivel ="none"
-        />
-      </main>
-      <Footer />
-    </>
-  );
-};
+
+    const [genero, setGenero] = useState("");
+    function alerta(icone, mensagem){
+      const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  }
+});
+Toast.fire({
+  icon: "icone",
+  title: "mensagem"
+});
+    }
+
+       async function cadastrarGenero(evt){
+        evt.preventDefault();
+        //verificar se o input esta vindo vazio
+        // trim: apaga os espaços
+        if(genero.trim() != ""){
+            
+            try{
+                //cadastrar um genero: post
+                await api.post("genero",{nome: genero});
+                 alerta("sucess", "Cadastro realizado com sucesso")
+                
+            }catch(error){
+                alerta("error", "Erro! entre em contato com o nosso suporte")
+                console.log(error);
+            }
+        }else{
+           alerta("info", "Deu ruim")
+        }
+
+    //try => tentar(o esperado)
+    //catch => pega a exceção
+
+
+    }
+
+    //TESTE: validar o genero
+    // useEffect(() => {
+    //     console.log(genero);
+    // },[genero]);
+    //fim do teste
+
+    return(
+        <>
+           <Header/>
+           <main>
+                <Cadastro tituloCadastro="Cadastro de Gênero"
+                visibilidade = "none"
+                placeholder = "gênero"
+
+                //atribuindo a função:
+                funcCadastro = {cadastrarGenero}
+                //atribuindo o valor do input:
+                valorInput = {genero}
+                //atibuindo a função que atualiza o meu genero
+                setValorInput ={setGenero}
+                />
+                
+                <Lista ListaTitulo="Lista de Gêneros"
+                visible="none"
+                />
+           </main>
+           <Footer/>
+       </> 
+    )
+}
 
 export default CadastroGenero;
